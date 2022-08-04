d8c79e251e6cd48aa29784394c44cd3ba9b02b99
Irene <irenemanningm@gmail.com>
2022-08-04 00:41:02 -0500

    commit tree c4ec5110746b215529b7718425c20782b723428e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "." 
+      true
+    else false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else false
+    end
   end
 
   def count_sentences
-
+    self_array = self.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s/)
+    #binding.pry
+    self_array.length
   end
 end
